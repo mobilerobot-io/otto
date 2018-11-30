40ac5f477888b76ebcf786f0f055dbfd88792cee
Rusty Eddy <rusty@rustyeddy.com>
2018-11-29 18:13:16 -0800

    create better docs

--- src/api.go
+++ src/api.go
@@ -1,3 +1,8 @@
+/*
+Otto is a simple tool to help keep track of the online inventory
+we are managing.  Monitors inventory, and launches tools like terraform
+and ansible to ensure our infrastructure is operating correctly.
+*/
 package otto
 
 import "github.com/gorilla/mux"
