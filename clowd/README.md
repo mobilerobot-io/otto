e13e233593812ad58c2eaa3cfebf34389b03ea9d
Rusty Eddy <rusty@rustyeddy.com>
2018-12-04 22:02:36 -0800

    clowd

---
+++ clowd/README.md
@@ -0,0 +1,11 @@
+# Clowds Ops Network
+
+Workflow ~ Initial
+
+1. Provision images
+
+- hosts: otto  & w01
+  - apt-update
+  - install openssh, python
+  
+
---
+++ clowd/tf/www.tf
@@ -0,0 +1,34 @@
+resource "digitalocean_droplet" "w01" {
+  name = "w01"
+  image = "ubuntu-18-04-x64"
+  region = "sfo2"
+  size = "s-1vcpu-1gb"
+
+  private_networking = true
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
+
+  connection {
+    user = "root"
+    type = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    timeout = "2m"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${digitalocean_droplet.w01.ipv4_address} >  /srv/inventory/clops/hosts-w01"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "sudo apt-get update",
+      "sudo apt-get -y install python"
+    ]
+  }
+}
+
+output "w01" {
+  value = "${digitalocean_droplet.w01.ipv4_address}"
+}
